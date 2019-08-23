633cb4780c5609e82a988399304908c4433e39f2
Capp Wiedenhoefer <cappw1@gmail.com>
2019-08-22 17:37:52 -0700

    Improve /filteredimage endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import bodyParser from "body-parser";
-import express from "express";
-import {deleteLocalFiles, filterImageFromURL} from "./util/util";
+import express, { Request, Response, Router } from "express";
+import { remove } from "fs-extra";
+import { isWebUri } from "valid-url";
+import { deleteLocalFiles, filterImageFromURL } from "./util/util";
 
 (async () => {
 
@@ -13,27 +15,20 @@ import {deleteLocalFiles, filterImageFromURL} from "./util/util";
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  app.get("/filteredimage", async ( req, res ) => {
-    const image_url = req.query.image_url;
-    res.send(image_url);
+  app.get("/filteredimage", async ( req: Request, res: Response ): Promise<void> => {
+    const { image_url } = req.query;
+    if ( !image_url || !isWebUri(image_url) ) {
+      res.status(400).send("a valid image_url is required");
+    } else {
+      const filteredImage = await filterImageFromURL( image_url );
+      res.status(200).sendFile(filteredImage);
+      remove(__dirname + "/util/tmp");
+    }
   });
 
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get( "/", async ( req: Request, res: Response ): Promise<void> => {
     res.send("try GET /filteredimage?image_url={{}}");
   });
 
